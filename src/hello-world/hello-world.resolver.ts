769749a61420feb55e123fe73ede082a5435d2b4
Alexander <alexandertakepics@gmail.com>
2023-05-23 10:53:33 +0200

    first endpoint hello world

---
+++ src/hello-world/hello-world.resolver.ts
@@ -0,0 +1,10 @@
+import { Resolver, Query } from '@nestjs/graphql';
+
+@Resolver()
+export class HelloWorldResolver {
+
+    @Query(() => String)
+    helloWorld(): string{
+        return 'Hello World'
+    }
+}
